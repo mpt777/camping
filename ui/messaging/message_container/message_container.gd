82a0df85c59bf99a74731f902978f231a0070eb6
mpt777 <mptaylor777@gmail.com>
2025-08-14 21:53:45 -0600

    smk resources

--- ui/messaging/message_container/message_container.gd
+++ ui/messaging/message_container/message_container.gd
@@ -6,9 +6,14 @@ const MESSAGE_UI = preload("res://ui/messaging/message_ui/message_ui.tscn")
 @onready var n_messages : VBoxContainer = %Messages
 @onready var n_text : LineEdit = %TextEdit
 
+const modulate_hidden : Color = Color("ffffff9e")
+const modulate_shown : Color = Color("ffffff")
+
 func _ready():
 	Signals.AddMessageToBox.connect(add_message)
 	self.ready.connect(_on_ready)
+	
+	self.modulate = self.modulate_hidden
 	
 func _on_ready():
 	self.set_multiplayer_authority(self.get_parent().get_multiplayer_authority())
@@ -18,21 +23,30 @@ func add_message(message : Message):
 	n_messages.add_child(message_ui, true)
 	message_ui.render()
 	self.n_messages.get_parent().scroll_vertical = INF
+	
+
+func activate():
+	Signals.SetInputMode.emit(["movement", "camera", "ui"], false)
+	self.modulate = self.modulate_shown
+	
+func deactive():
+	Signals.SetInputMode.emit(["movement", "camera", "ui"], true)
+	self.modulate = self.modulate_hidden
 
 # Text Input
 func _on_text_edit_focus_entered() -> void:
 	if is_multiplayer_authority():
-		Signals.SetInputMode.emit(["movement", "camera", "ui"], false)
+		self.activate()
 
 func _on_text_edit_focus_exited() -> void:
 	if is_multiplayer_authority():
-		Signals.SetInputMode.emit(["movement", "camera", "ui"], true)
+		self.deactive()
 
 func _input(event: InputEvent) -> void:
 	if is_multiplayer_authority():
 		if event.is_action_pressed("exit"):
 			accept_event()
-			Signals.SetInputMode.emit(["movement", "camera", "ui"], true)
+			self.deactive()
 			n_text.release_focus()
 
 func _on_text_edit_text_submitted(new_text: String) -> void:
